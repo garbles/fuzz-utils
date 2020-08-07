69c3da1e09710012ea09be56666e62b65ed6aa39
Gabe Scholz <github@gabe.pizza>
2020-08-07 13:54:14 -0700

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,4 @@
-const path = require("path");
-
-const rootDir = path.join(process.cwd(), "src");
-const tsConfigPath = path.join(process.cwd(), "tsconfig.json");
-const tsConfig = require(tsConfigPath);
-
 module.exports = {
-  verbose: true,
-  rootDir,
-  testEnvironment: "node",
-  transform: {
-    "\\.ts$": "ts-jest"
-  },
-  globals: {
-    "ts-jest": {
-      tsConfig: tsConfig.compilerOptions
-    }
-  }
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
